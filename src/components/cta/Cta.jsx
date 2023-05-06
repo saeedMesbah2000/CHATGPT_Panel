16b27b98bf00c84f82d0022449c9ff9bdf3d2dc6
saeed <saeed@mary.local>
2023-05-06 23:36:07 +0330

    first commit

---
+++ src/components/cta/Cta.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./cta.css";
+
+const Cta = () => {
+  return (
+    <div className="gpt3__cta section__padding">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="gpt3__cta-button">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cta;
